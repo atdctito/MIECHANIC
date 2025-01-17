51414fbfb5caff6f1586991331615b8c85ea3ab0
atdctito <atdc169@gmail.com>
2025-01-17 22:34:11 +0700

    Add files via upload

---
+++ src/components/Navbar.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { useKeranjang } from '../context/KeranjangContext';
+import { FaShoppingCart } from 'react-icons/fa'; // Mengimpor ikon keranjang
+import '../styles/Navbar.css'; // Mengimpor file CSS
+
+function Navbar() {
+  const { keranjang } = useKeranjang();
+
+  // Hitung total item di keranjang
+  const totalItem = keranjang.reduce((total, item) => total + item.jumlah, 0);
+
+  return (
+    <nav className="navbar">
+      <div className="container">
+        <Link className="navbar-brand" to="/">Korean Spicy</Link>
+        
+        {/* Menu Navigasi */}
+        <ul className="navbar-nav">
+          <li className="nav-item">
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link" to="/menu">MENU</Link>
+          </li>
+        </ul>
+
+        {/* Button Klik pada Logo Keranjang */}
+        <Link to="/keranjang" className="cart-icon">
+          <FaShoppingCart size={24} />
+          {totalItem > 0 && <span className="badge">{totalItem}</span>} {/* Tampilkan jumlah item */}
+        </Link>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
